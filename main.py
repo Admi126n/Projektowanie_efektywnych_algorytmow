07c946a12dd3f144944bb0be3c07e4dfd9f86971
Adam Tokarski <a.tokarski126@gmail.com>
2022-10-15 23:18:43 +0100

    start writing functions

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import csv
+import sys
+import itertools
 
 
-def read_test_data(csv_patch):
-    with open(csv_patch) as file:
+def read_test_data(file_path):
+    with open(file_path) as file:
+        things = csv.reader(file, delimiter=' ')
+        next(things)
+        things = list(things)
+    # change data from string to int
+    for i in range(len(things)):
+        for j in range(len(things[i])):
+            things[i][j] = int(things[i][j])
+    return things
+
+
+def read_ini(ini_path):
+    with open(ini_path) as file:
         things = csv.reader(file, delimiter=' ')
         next(things)
         things = list(things)
     return things
-
-
-def read_ini():
-    pass
 
 
 def write_output():
@@ -21,4 +31,19 @@ def tsp_brute_force():
     pass
 
 
-print("Hello PEA!")
+def main(ini_path):
+    graphs_to_check = read_ini(ini_path)
+    output_file_path = graphs_to_check.pop()
+    for graph in graphs_to_check:
+        graph_path = graph[0]
+        iterations = int(graph[1])
+        for i in range(iterations):
+            print(i)
+    # print(graphs_to_check)
+    # print(output_file_path)
+
+
+ini = ".ini"
+
+# main(ini)
+print(read_test_data(r"Test_data\tsp_6_1.txt"))

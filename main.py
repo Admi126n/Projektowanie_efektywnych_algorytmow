04ff453f3fc7bd4ed896c3eb5cc54b717ac5611b
Adam Tokarski <a.tokarski126@gmail.com>
2022-10-15 22:30:16 +0100

    add test data and ini

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import csv
+
+
+def read_test_data(csv_patch):
+    with open(csv_patch) as file:
+        things = csv.reader(file, delimiter=' ')
+        next(things)
+        things = list(things)
+    return things
+
+
+def read_ini():
+    pass
+
+
+def write_output():
+    pass
+
+
+def tsp_brute_force():
+    pass
+
+
 print("Hello PEA!")
